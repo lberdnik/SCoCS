10370e769a9a993de04c7f71d9a6b39217676ff2
Lyubov <lberdnikk2@gmail.com>
2023-04-25 20:33:59 +0300

    add constants.py

---
+++ task2/constants.py
@@ -0,0 +1,6 @@
+COMMANDS_HELP = "List of commands:\nadd <key> [key, ...] - add one or more elements to the container (if the element \
+        is already in there then don’t add)\nremove <key> - delete key from container\nfind <key> [key, \
+            ...] - check if the element is presented in the container, print each found or \'No such elements\' if \
+                nothing is\nlist - print all elements of container\ngrep <regex> - check the value in the container by \
+                    regular expression, print each found or \'No such elements\' if nothing is\nsave - save container to \
+                        file\nload - load container from file\nswitch - switches to another user\nexit - exit the program"
