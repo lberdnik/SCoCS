bb4f26c74e6223ac49a2351dd9c1ae388e7b3257
Lyubov <lberdnikk2@gmail.com>
2023-06-04 22:24:24 +0300

    add templates: base.html, home.html

--- ZooApp/zoo/views.py
+++ ZooApp/zoo/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
-# Create your views here.
+def index(request):
+    return render(request, 'zoo/home.html')
